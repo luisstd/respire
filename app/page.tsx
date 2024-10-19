b4e67a5684c1b14ff9748611dc409b49ac917778
Luis Steidle <luis@steidle.me>
2024-10-19 19:56:06 +0200

    refactor: update copy

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid place-items-center min-h-screen">
       <main className="flex flex-col items-center gap-32">
         <div className="flex items-center gap-3 text-2xl font-sans font-bold">
-          <p>Respire - Breathing app</p>
+          <p>Breathe with the circle</p>
         </div>
         <Breathing />
       </main>

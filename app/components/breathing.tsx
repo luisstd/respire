a6d1f5c8a1348afc257ebdaad204d46bc51888bd
Luis Steidle <luis@steidle.me>
2024-10-19 19:50:49 +0200

    refactor: update motion div

--- app/components/breathing.tsx
+++ app/components/breathing.tsx
@@ -3,15 +3,11 @@ import { motion } from "framer-motion";
 
 export const Breathing: React.FC = () => {
   return (
-    <>
-      <motion.div
-        className="w-20 h-20 bg-primary rounded-full"
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-        }}
-      />
-    </>
+    <motion.div
+      className="w-64 h-64 bg-primary rounded-full"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    />
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       <main className="flex flex-col items-center gap-32">
         <div className="flex items-center gap-3 text-2xl font-sans font-bold">
           <p>Respire - Breathing app</p>
-          <div className="w-5 h-5 bg-primary rounded-full" />
         </div>
         <Breathing />
       </main>

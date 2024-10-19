5cae46fa0043961584c6d96066647be5c9434288
Luis Steidle <luis@steidle.me>
2024-10-19 18:28:48 +0200

    feat: configure primary color

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
     <div className="grid place-items-center min-h-screen ">
-      <main className="text-2xl font-sans font-bold">
-        Respire - Breathing app
+      <main className="flex items-center gap-3 text-2xl font-sans font-bold">
+        <p>Respire - Breathing app</p>
+        <div className="w-5 h-5 bg-primary rounded-full" />
       </main>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
       },
     },
   },

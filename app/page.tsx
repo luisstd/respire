bd2a354bc041731dae5188f06c6160fdd7c79e74
Luis Steidle <luis@steidle.me>
2025-01-04 15:07:55 +0300

    refactor: disable overflow scroll

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,16 @@ export default function Home() {
 	const [speed, setSpeed] = useState(7);
 
 	return (
-		<div className="grid place-items-center min-h-screen">
-			<main className="flex flex-col items-center gap-32">
+		<div className="grid place-items-center min-h-screen max-h-screen overflow-hidden p-5">
+			<main className="flex flex-col items-center gap-24 md:gap-36">
 				<div className="flex items-center gap-3 text-2xl font-sans font-bold">
 					<p>Breathe with the circle</p>
 				</div>
 				<Breathing breathsPerMinute={speed} />
-				<Speed breathsPerMinute={speed} handleSpeedChange={setSpeed} />
+				<div className="flex justify-between w-full">
+					<Speed breathsPerMinute={speed} handleSpeedChange={setSpeed} />
+					<Speed breathsPerMinute={speed} handleSpeedChange={setSpeed} />
+				</div>
 				{/* <Menu /> */}
 			</main>
 		</div>

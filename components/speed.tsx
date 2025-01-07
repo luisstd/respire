4e685d3dfca95b9edaf374f32950722015d8f5d8
Luis Steidle <luis@steidle.me>
2025-01-07 22:06:36 +0100

    fix: wrap speed buttons on very small screens

--- components/speed.tsx
+++ components/speed.tsx
@@ -39,7 +39,7 @@ export const Speed: React.FC<SpeedProps> = ({ handleSpeedChange }) => {
 					<DrawerDescription>Select breaths per minute</DrawerDescription>
 				</DrawerHeader>
 				<DrawerFooter>
-					<div className="flex gap-2 pb-10 mx-auto">
+					<div className="flex flex-wrap gap-2 pb-10 mx-auto">
 						{speeds.map((speed) => (
 							<Button
 								key={speed}

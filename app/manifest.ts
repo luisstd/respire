2c352885f8106182b97f267a624b7c48acfd2445
Luis Steidle <luis@steidle.me>
2025-01-21 23:00:23 +0100

    chore: rename manifest image links

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#BF52B6",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },

be4433839fa1862c3f2ee8d76f1adb4c2b67aefc
Luis Steidle <luis@steidle.me>
2025-01-21 22:52:03 +0100

    feat: add manifest file & image assets

---
+++ app/manifest.ts
@@ -0,0 +1,26 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Respire - Breathing App",
+    short_name: "Respire",
+    description:
+      "Respire is a tool for breathing exercises. Simply breathe with the circle.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#BF52B6",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}

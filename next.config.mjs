ec82beaeaf376df49f69ae56833b3228bc4cb129
Luis Steidle <luis@steidle.me>
2025-01-22 22:05:40 +0100

    chore: enable offline fallback

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,12 @@ const withSerwist = withSerwistInit({
 	swSrc: "app/sw.ts",
 	swDest: "public/sw.js",
 	reloadOnOnline: true,
+	cacheOnNavigation: true,
+	additionalPrecacheEntries: [{ url: "/" }],
 });
 
-const nextConfig = withSerwist({});
+const nextConfig = withSerwist({
+	reactStrictMode: true,
+});
 
 export default nextConfig;

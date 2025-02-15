af36373b5a22e8e4b5ace0f6924993895663885c
Luis Steidle <luis@steidle.me>
2025-02-15 18:12:05 +0100

    refactor: update icons in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -34,11 +34,6 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: "maskable",
       },
       {
-        src: "/web-app-manifest-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
         src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
